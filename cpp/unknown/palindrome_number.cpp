6aae9c6d8f475514454518666352b1b05d350395
KABILAN S <119160889+S-KABILAN@users.noreply.github.com>
2025-05-13 16:59:02 +0530

    Add solution for Palindrome Number

--- cpp/unknown/palindrome_number.cpp
+++ cpp/unknown/palindrome_number.cpp
@@ -1,15 +1,24 @@
-#include <string>
+#include <string>
 
-class Solution {
+class Solution {
 public:
-    bool isPalindrome(int x) {
-        if (x < 0) {
-            return false;
-        }        
-        // Convert integer to string
-        std::string str = std::to_string(x);
-        int left = 0;
-        int right = str.length() - 1;
-        
-        while (left < right) {
-            if (str[left] != str[right]) {
+    bool isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }        
+        // Convert integer to string
+        std::string str = std::to_string(x);
+        int left = 0;
+        int right = str.length() - 1;
+        
+        while (left < right) {
+            if (str[left] != str[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+};
